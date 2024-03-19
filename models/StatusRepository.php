270d224a769923ec1432c4fc18ed07cd00ae4aea
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-03-19 19:19:09 +0900

    ユーザーの投稿一覧と投稿詳細作成完了

--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 
-class StatusRepository extends DbRepository {
-    public function insert($user_id, $body) {
+class StatusRepository extends DbRepository
+{
+    public function insert($user_id, $body)
+    {
         $now = new DateTime();
 
         $sql = "
@@ -16,7 +18,8 @@ class StatusRepository extends DbRepository {
         ));
     }
 
-    public function fetchAllPersonalArchivesByUserId($user_id){
+    public function fetchAllPersonalArchivesByUserId($user_id)
+    {
         $sql = "
             SELECT a.*, u.user_name
                 FROM status a 
@@ -26,5 +29,34 @@ class StatusRepository extends DbRepository {
         ";
 
         return $this->fetchAll($sql, array(':user_id' => $user_id));
+    }
+
+    public function fetchAllByUserId($user_id)
+    {
+        $sql = "
+            SELECT a.*, u.user_name
+                FROM status a
+                    LEFT JOIN user u ON a.user_id = u.id
+                WHERE u.id = :user_id
+                ORDER BY a.created_at DESC
+            ";
+
+        return $this->fetchAll($sql, array(':user_id' => $user_id));
+    }
+
+    public function fetchByIdAndUserName($id, $user_name)
+    {
+        $sql = "
+            SELECT a.* , u.user_name
+                FROM status a 
+                    LEFT JOIN user u ON u.id = a.user_id
+                WHERE a.id = :id
+                    AND u.user_name = :user_name
+        ";
+
+        return $this->fetch($sql, array(
+            ':id' => $id,
+            ':user_name' => $user_name,
+        ));
     }
 }
